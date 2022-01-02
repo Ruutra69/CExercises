f2ce59961243bb6618852ff5d0a1a51b9cb21727
Ruutra69 <95411160+Ruutra69@users.noreply.github.com>
2022-01-02 10:59:27 +0100

    Add files via upload

--- slaetsArtuur_9_7.c
+++ slaetsArtuur_9_7.c
@@ -1,4 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-main(void)
+int main()
+  {
+  int i;
+  
+  for(i=0; i<10; i++)
+    {
+    printf("Enter student name #%d: ", i+1);
+    fgets(name[i], 80, stdin);
+    name[i][strlen(name[i])-1] = '\0';  
+    }
+
+  printf("\nThe names of the students are:\n");
+  for(i=0; i<n; i++)
+    printf("name[%d] = \"%s\"\n", i, name[i]);
+  return 0;
+  }
