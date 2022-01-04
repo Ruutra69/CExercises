0ba841253a31e91815e4efc763472022e2500009
Ruutra69 <95411160+Ruutra69@users.noreply.github.com>
2022-01-04 13:53:38 +0100

    Add files via upload

---
+++ slaetsArtuur_5_3_16.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<Windows.h>
+
+int main(void) {
+	printf("Dit programma bouwt een omgekeerde toren!! \n");	
+	torenbouwen();
+}
+int torenbouwen(void)
+{
+	int i, j, k, l, m;
+	
+	printf("Geef een getal voor de zijde van de eerste vierkant:\n");
+	scanf("%d", &k);
+	m = 0;
+	for(l=k; l > 0;)
+	{
+		
+		for(j=0;j<=l;)
+		{
+			for(i=0;i<=l;)
+			{
+				printf("■");
+				i += 1;
+				
+			}
+			if ( m > 0)
+			{
+				printf(" ");
+				m -= 1;
+			}
+			printf("\n");
+			j += 1;
+			
+		}
+		j, i = 0;
+		m += 1;
+		l -= 2;
+		
+	}
+}
